7e1b8f917894eae6ca8914f2c3ec8386744f4cc0
Guillermo Álvarez <guillermo@cientifico.net>
2013-04-17 17:03:47 +0200

    Make it work

--- lib/highline/wizard.rb
+++ lib/highline/wizard.rb
@@ -1,20 +1,55 @@
 require "highline/wizard/version"
+require 'highline'
 
 module Highline
   module Wizard
     class Wizzard
+      attr_reader :selected
       def initialize(data)
-        options = Struct.new(:titles).new
-        yield options if &blockgiven
-        @selected = (0..(data.size)).to_a
-        columns = data.map(&:size).max
+        @options = Struct.new(:titles, :data).new
+        @options.data = data
+        @options.data.freeze
+        @hl = HighLine.new
+        yield @options if block_given?
 
+        result = []
+        current_data = data
+        columns_count.times do |column_index|
+          values = current_data.transpose[column_index].uniq
+          choosen = nil
+          if values.size > 1
+            elected = choose(values, @options.titles[column_index])
+          else
+            elected = values.first
+          end
 
-        while @selected.size > 1 || questions_left < 1
+          result << elected
 
+          current_data = data.select{|row|
+            row[column_index] == elected
+          }
 
         end
+        @selected = result
+      end
 
+      def choose(values, title)
+        @hl.choose(*values) do  |menu|
+          menu.prompt = "Please choose #{title}" if title
+        end
+      end
+
+      protected
+      def data
+        @options.data
+      end
+
+      def columns_count
+        columns.size
+      end
+
+      def columns
+        data.transpose
       end
     end
 
